bd65121ab80a1a9d592448a25a93c28a9df6a015
MathisZerbib <mathis.zerbib@gmail.com>
2024-09-19 17:47:29 +0200

    failed to deplaoy due to types check linter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Session } from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string;
+      id?: string | null;
       name?: string | null;
       email?: string | null;
       image?: string | null;
